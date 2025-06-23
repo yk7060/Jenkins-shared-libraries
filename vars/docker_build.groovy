b5e1e8f5a48d524db35d416c768e2e395dd63a81
yk7060 <ykathar132@gmail.com>
2025-06-23 17:22:51 +0530

    docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/$ProjectName}:${ImageTag} ."
+def call(String ImageTag, String DockerHubUser) {
+    sh "docker build -t ${DockerHubUser}/${env.ProjectName}:${ImageTag} ."
 }
